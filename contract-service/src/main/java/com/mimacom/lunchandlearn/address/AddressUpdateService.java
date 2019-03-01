802e17db914b08fd1dce8f0f466999ed05cb3a44
Zoltan Altfatter <altfatterz@gmail.com>
2019-03-01 14:24:44 +0100

    cleanup contract price update

--- contract-service/src/main/java/com/mimacom/lunchandlearn/address/AddressUpdateService.java
+++ contract-service/src/main/java/com/mimacom/lunchandlearn/address/AddressUpdateService.java
@@ -3,20 +3,8 @@ package com.mimacom.lunchandlearn.address;
 import com.mimacom.lunchandlearn.ContractRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
 @Service
 public class AddressUpdateService {
-
-    private final Set<String> ZURICH_POSTCODES = new HashSet<>(Arrays.asList(
-            "8057", "8045", "8008", "8001", "8003", "8038", "8049", "8064"
-            ));
-
-    private final Set<String> BERN_POSTCODES = new HashSet<>(Arrays.asList(
-            "3001", "3006", "3010", "3013", "3018", "3027", "3011", "3014"
-    ));
 
     private final ContractRepository contractRepository;
 
@@ -27,22 +15,20 @@ public class AddressUpdateService {
     public void process(AddressUpdateEvent event) {
 
         if (movingFromZurichToBern(event)) {
-            contractRepository.updateBasicInsurancePrice(event.getCustomerId(), 30.0);
+            contractRepository.updateBasicInsurancePrice(event.getCustomerId(), 25.0);
         }
         if (movingFromBernToZurich(event)) {
-            contractRepository.updateBasicInsurancePrice(event.getCustomerId(), -30.0);
+            contractRepository.updateBasicInsurancePrice(event.getCustomerId(), -25.0);
         }
 
     }
 
     private boolean movingFromZurichToBern(AddressUpdateEvent event) {
-        return ZURICH_POSTCODES.contains(event.getCurrentAddress().getPostCode())
-                && (BERN_POSTCODES.contains(event.getNewAddress().getPostCode()));
+        return "Zurich".equals(event.getCurrentAddress().getCity()) && "Bern".equals(event.getNewAddress().getCity());
     }
 
     private boolean movingFromBernToZurich(AddressUpdateEvent event) {
-        return BERN_POSTCODES.contains(event.getCurrentAddress().getPostCode()) &&
-                (ZURICH_POSTCODES.contains(event.getNewAddress().getPostCode()));
+        return "Bern".equals(event.getCurrentAddress().getCity()) && "Zurich".equals(event.getNewAddress().getCity());
     }
 
 }
